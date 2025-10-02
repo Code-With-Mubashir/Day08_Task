1218cfbeefd94438505d49595d04fbe4fc085a16
Mubashir Rasool <bc220207658mra@vu.edu.pk>
2025-10-02 23:45:45 +0500

    Add files via upload

---
+++ Day08_Task.py
@@ -0,0 +1,39 @@
+# What is module?
+# EXample No.01
+import math
+print(math.sqrt(16))
+
+# Example No.02
+print(math.pi)
+
+# Example No.03
+import random
+print(random.randint(1,10))
+
+# Example No.04
+import datetime
+now = datetime.datetime.now
+print(now)
+
+# Example No.05
+import math
+print(math.pi)
+
+# Example No.06
+import os
+print(os.name)
+print(os.getcwd())
+
+# Example No.07
+import sys
+print(sys.version)
+
+# Example No.08
+import time
+print("Wait for 2 seconds...")
+time.sleep(2)
+print("Done")
+# Example No.09
+help("module")
+
+
